a5bc0fedfa6d398659cd2b30552eaba1788faf58
daemunozr <dae.munzor@duocuc.cl>
2026-03-29 21:50:02 -0300

    first commit

---
+++ src/main/java/com/example/fitlifetech/FitlifetechApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitlifetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlifetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlifetechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fitlifetech/FitlifetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitlifetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlifetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
