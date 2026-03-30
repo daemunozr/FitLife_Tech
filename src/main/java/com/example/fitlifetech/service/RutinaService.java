f7e6425e1af8ac53760aa7e4c85c0248558efe29
daemunozr <dae.munozr@duocuc.cl>
2026-03-30 20:57:57 -0300

    splitted generic methods

--- src/main/java/com/example/fitlifetech/service/RutinaService.java
+++ src/main/java/com/example/fitlifetech/service/RutinaService.java
@@ -18,20 +18,24 @@ public class RutinaService {
                 return repo.obtenerRutinas();
         }
 
-        public List<Rutina> obtenerRutinasOrdenadas(String criterio) throws Exception{
-            try {
-                switch (criterio) {
-                        case ("id")             -> repo.obtenerRutinasOrdenadasPorId();
-                        case ("nombre")         -> repo.obtenerRutinasOrdenadasPorNombre();
-                        case ("nivel")          -> repo.obtenerRutinasOrdenadasPorNivel();
-                        case ("duracion")       -> repo.obtenerRutinasOrdenadasPorDuracion();
-                        case ("entrenador")     -> repo.obtenerRutinasOrdenadasPorEntrenador();
-                        default -> throw new Exception("criterio " + criterio + " no valido");
-                }   
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-            return null;
+        public List<Rutina> obtenerRutinasOrdenadasPorId(){
+                return repo.obtenerRutinasOrdenadasPorId();
+        }
+
+        public List<Rutina> obtenerRutinasOrdenadasPorNombre(){
+                return repo.obtenerRutinasOrdenadasPorNombre();
+        }
+
+        public List<Rutina> obtenerRutinasOrdenadasPorNivel(){
+                return repo.obtenerRutinasOrdenadasPorNivel();
+        }
+
+        public List<Rutina> obtenerRutinasOrdenadasPorDuracion(){
+                return repo.obtenerRutinasOrdenadasPorDuracion();
+        }
+
+        public List<Rutina> obtenerRutinasOrdenadasPorEntrenador(){
+                return repo.obtenerRutinasOrdenadasPorEntrenador();
         }
 
         public List<Rutina> obtenerRutinasCortas(){
@@ -50,46 +54,58 @@ public class RutinaService {
                 return repo.obtenerRutinasDificiles();
         }
 
-        public <E> Rutina buscarPorIdentificador(String identificador,E element) throws Exception{
-                
-                try{
-                        switch (identificador){
-                        case ("id")     -> repo.buscarPorId((int)element);
-                        case("nombre")  -> repo.buscarPorNombre((String)element);
-                        default -> throw new Exception("identificador " + identificador + " invalido");
-                        }
-                }catch (ClassCastException e){
-                        System.out.println("Error: elemento " + e.getMessage());
-                }
-                return null;
+        public Rutina buscarPorId(int id) throws Exception{
+                Rutina tmp_rutina = repo.buscarPorId(id); 
+                if(tmp_rutina == null)
+                        throw new Exception("Error: rutina no encontrada por id " + id);
+                return tmp_rutina;
         }
 
-        public <E> List<Rutina> buscarPorCriterio(String criterio, E element) throws Exception{
+        public Rutina buscarPorNombre(String nombre) throws Exception{
+                Rutina tmp_rutina = repo.buscarPorNombre(nombre);
+                if(tmp_rutina == null)
+                        throw new Exception("Error: rutina no encontrada por nombre " + nombre);
+                return tmp_rutina;
+        }
 
-                try {
-                        switch (criterio) {
-                            case ("nivel")      -> repo.buscarPorNivel((int)element);
-                            case ("duracion")   -> repo.buscarPorDuracion((int)element);
-                            case ("tipo")       -> repo.buscarPorTipo((String)element);
-                            case ("entrenador") -> repo.buscarPorEntrenador((String)element);
-                            default             -> throw new Exception("criterio " + criterio + " invalido");
-                        }
-                } catch (ClassCastException e) {
-                        System.out.println("Error: elemento " + e.getMessage());
-                }
-                return null;
+        public List<Rutina> buscarPorNivel(int nivel) throws Exception{
+                List<Rutina> tmp_rutinas = repo.buscarPorNivel(nivel);
+                if(tmp_rutinas == null)
+                        throw new Exception("Error: rutina no encontrada por nivel " + nivel);
+                return tmp_rutinas;
+        }
+
+        public List<Rutina> buscarPorDuracion(int duracion) throws Exception{
+                List<Rutina> tmp_rutinas = repo.buscarPorDuracion(duracion);
+                if(tmp_rutinas == null)
+                        throw new Exception("Error: rutina no encontrada por duracion " + duracion);
+                return tmp_rutinas;
+        }
+
+        public List<Rutina> buscarPorTipo(String tipo) throws Exception{
+                List<Rutina> tmp_rutinas = repo.buscarPorTipo(tipo);
+                if(tmp_rutinas == null)
+                        throw new Exception("Error: rutina no encontrada por tipo " + tipo);
+                return tmp_rutinas;
+        }
+
+        public List<Rutina> buscarPorEtrenador(String entrenador) throws Exception{
+                List<Rutina> tmp_rutinas = repo.buscarPorEntrenador(entrenador);
+                if(tmp_rutinas == null)
+                        throw new Exception("Error: rutina no encontrada por entrenador " + entrenador);
+                return tmp_rutinas;
         }
 
         public Rutina actualizarRutina(Rutina rutina) throws Exception{
                 if(repo.actualizar(rutina) == null)
-                        throw new Exception("Error: rutina no encontrada para actualizar");
+                        throw new Exception("Error: rutina " + rutina.getNombre() + " con id " + rutina.getId() + " no encontrada para actualizar");
 
                 return rutina;
         }
 
         public String borrarRutina(int id) throws Exception{
                 if(repo.buscarPorId(id) == null)
-                        throw new Exception("Error: rutina no encontrada para actualizar");
+                        throw new Exception("Error: rutina con id " + id + " no encontrada para borrar");
 
                 repo.eliminar(id);
                 return "Rutina eliminada";
@@ -97,7 +113,7 @@ public class RutinaService {
 
         public String borrarRutina(String nombre) throws Exception{
                 if(repo.buscarPorNombre(nombre) == null)
-                        throw new Exception("Error: rutina no encontrada para actualizar");
+                        throw new Exception("Error: rutina con nombre " + nombre + " no encontrada para borrar");
 
                 repo.eliminar(nombre);
                 return "Rutina eliminada";
